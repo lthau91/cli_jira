c8354648124b07fe40d9d229f86f43d6f6f3666e
Hau Lam <lthau91@gmail.com>
2023-03-05 00:25:11 +0800

    Stage 1 databases and models

---
+++ src/models.rs
@@ -0,0 +1,50 @@
+use serde::{Serialize, Deserialize};
+use std::{collections::HashMap, fmt::Display};
+
+#[derive(Serialize, Deserialize, PartialEq, Eq, Debug, Clone)]
+pub enum Status {
+    Open,
+    InProgess,
+    Resolved,
+    Closed
+}
+#[derive(Serialize, Deserialize, PartialEq, Eq, Debug, Clone)]
+pub struct Epic {
+    pub name: String,
+    pub description: String,
+    pub status: Status,
+    pub stories: Vec<u32>,
+}
+
+impl Epic {
+    pub fn new(name: String, description: String) -> Self {
+        Self {
+            name,
+            description,
+            status: Status::Open,
+            stories: vec![]
+        }
+    }
+}
+#[derive(Serialize, Deserialize, PartialEq, Eq, Debug, Clone)]
+pub struct Story {
+    pub name: String,
+    pub description: String,
+    pub status: Status,
+}
+
+impl Story {
+    pub fn new(name: String, description: String) -> Self {
+        Self {
+            name,
+            description,
+            status: Status::Open,
+        }
+    }
+}
+#[derive(Serialize, Deserialize, PartialEq, Eq, Debug, Clone)]
+pub struct DBState {
+    pub last_item_id: u32,
+    pub epics: HashMap<u32, Epic>,
+    pub stories: HashMap<u32, Story>
+}
